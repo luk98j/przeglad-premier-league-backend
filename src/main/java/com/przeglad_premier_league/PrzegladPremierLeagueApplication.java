457069dd463809fe0955e5efb6f71d6184fb209f
Ukson <jankowskilukasz1337@gmail.com>
2021-03-09 14:44:51 +0100

    Initial commit

---
+++ src/main/java/com/przeglad_premier_league/PrzegladPremierLeagueApplication.java
@@ -0,0 +1,12 @@
+package com.przeglad_premier_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzegladPremierLeagueApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PrzegladPremierLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/przeglad_premier_league/PrzegladPremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przeglad_premier_league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzegladPremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
